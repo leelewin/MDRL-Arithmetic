b46ce5d3649f88fb4e0b8e9704bfc737b0b58171
LMingL <2438124738@qq.com>
2021-02-09 22:11:00 +0800

    finish qlearning

---
+++ RL/Q-learning_class.py
@@ -0,0 +1,12 @@
+class QLearning:
+    def __init__(self):
+        pass
+
+    def check_state_exist(self):
+        pass
+
+    def choose_action(self):
+        pass
+
+    def update(self):
+        pass
